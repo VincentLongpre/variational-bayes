22e5965be9e1575b720b4756d80a4c10e8ca6a17
Vincent Longpre <vincent.longpre1729@gmail.com>
2023-12-16 16:51:12 -0500

    random grid search

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from VariNet.utils.train import train_vae_MNIST, train_avae_toy, train_avae_MNIS
 from VariNet.models.vae import MnistVAE
 from VariNet.models.avae import MnistAVAE
 import json
+import random
 import torch
 from pathlib import Path
 
 # Device
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
 
 if __name__ == "__main__":
     # Create examples directory
@@ -14,172 +16,57 @@ if __name__ == "__main__":
 
     ##########################################################
     print('#'*50)
-    print('VAE Vanilla MNIST')
+    print('Random Grid Search')
 
-    try:
-        train_vae_MNIST(batch_size = 64,
-                    z_dim = 32,
-                    lr = 1e-3,
-                    epochs = 30,
-                    image_size = 32,
-                    input_channels = 1,
-                    results_folder = "results/vae_vanilla_MNIST",
-                    binary = False,
-                    decoder_features = 32,
-                    encoder_features = 32)
-    except:
-        print('Error in VAE Vanilla MNIST')
+    # Define ranges for hyperparameters
+    batch_sizes = [32, 64, 128]
+    z_dims = [16, 32, 64, 100]
+    lr_values = [5e-3, 1e-3, 5e-4, 1e-4]
+    adv_lr_values = [5e-3, 1e-3, 5e-4, 1e-4]
+    epoch_values = [2, 5, 10, 20, 30]
+    image_sizes = [28, 32]
+    channels = 1
+    binary_values = [0, 1]
+    decoder_features_list = [16, 32, 64]
+    encoder_features_list = [16, 32, 64]
 
-    ##########################################################
-    print('#'*50)
-    print('VAE Binary MNIST')
+    for i in range(4):  # Number of setups
+        print(f'Setup {i + 1}')
+        try:
+            # Randomly sample hyperparameters for each setup
+            batch_size = random.choice(batch_sizes)
+            z_dim = random.choice(z_dims)
+            lr = random.choice(lr_values)
+            adv_lr = random.choice(adv_lr_values)
+            epochs = random.choice(epoch_values)
+            image_size = random.choice(image_sizes)
+            binary = random.choice(binary_values)
+            decoder_features = random.choice(decoder_features_list)
+            encoder_features = random.choice(encoder_features_list)
 
-    try:
-        train_vae_MNIST(batch_size = 64,
-                    z_dim = 32,
-                    lr = 1e-3,
-                    epochs = 30,
-                    image_size = 32,
-                    input_channels = 1,
-                    results_folder = "results/vae_binary_MNIST",
-                    binary = True,
-                    decoder_features = 32,
-                    encoder_features = 32)
-    except:
-        print('Error in VAE Binary MNIST')
+            if i % 2 == 0:  # VAE setups
+                train_vae_MNIST(batch_size=batch_size,
+                                z_dim=z_dim,
+                                lr=lr,
+                                epochs=epochs,
+                                image_size=image_size,
+                                input_channels=channels,
+                                results_folder=f"results/vae_setup_{i + 1 if not i else i}",
+                                binary=binary,
+                                decoder_features=decoder_features,
+                                encoder_features=encoder_features)
+            else:  # AVAE setups
+                train_avae_MNIST(batch_size=batch_size,
+                                 z_dim=z_dim,
+                                 primal_lr=lr,
+                                 adv_lr=adv_lr,
+                                 epochs=epochs,
+                                 image_size=image_size,
+                                 input_channels=channels,
+                                 results_folder=f"results/avae_setup_{i}",
+                                 binary=binary,
+                                 decoder_features=decoder_features,
+                                 encoder_features=encoder_features)
 
-    ##########################################################
-    print('#'*50)
-    print('AVAE Vanilla MNIST')
-
-    print('Setup 1')
-    try:
-        train_avae_MNIST(batch_size = 64,
-                        z_dim = 16,
-                        primal_lr = 1e-3,
-                        adv_lr = 1e-4,
-                        epochs = 30,
-                        image_size = 16,
-                        input_channels = 1,
-                        results_folder = "results/avae_vanilla_mnist_1",
-                        binary = False,
-                        decoder_features = 16,
-                        encoder_features = 16)
-    except:
-        print('Error in AVAE Vanilla MNIST Setup 1')
-
-    print('Setup 2')
-    try:
-        train_avae_MNIST(batch_size = 64,
-                        z_dim = 32,
-                        primal_lr = 1e-3,
-                        adv_lr = 1e-4,
-                        epochs = 30,
-                        image_size = 32,
-                        input_channels = 1,
-                        results_folder = "results/avae_vanilla_mnist_2",
-                        binary = False,
-                        decoder_features = 32,
-                        encoder_features = 32)
-    except:
-        print('Error in AVAE Vanilla MNIST Setup 2')
-
-    print('Setup 3')
-    try:
-        train_avae_MNIST(batch_size = 64,
-                        z_dim = 32,
-                        primal_lr = 1e-4,
-                        adv_lr = 5e-4,
-                        epochs = 40,
-                        image_size = 32,
-                        input_channels = 1,
-                        results_folder = "results/avae_vanilla_mnist_3",
-                        binary = False,
-                        decoder_features = 32,
-                        encoder_features = 32)
-    except:
-        print('Error in AVAE Vanilla MNIST Setup 3')
-
-    print('Setup 4')
-    try:
-        train_avae_MNIST(batch_size = 64,
-                        z_dim = 64,
-                        primal_lr = 1e-3,
-                        adv_lr = 1e-4,
-                        epochs = 40,
-                        image_size = 32,
-                        input_channels = 1,
-                        results_folder = "results/avae_vanilla_mnist_4",
-                        binary = False,
-                        decoder_features = 64,
-                        encoder_features = 64)
-    except:
-        print('Error in AVAE Vanilla MNIST Setup 4')
-
-    ##########################################################
-    print('#'*50)
-    print('AVAE Binary MNIST')
-
-    print('Setup 1')
-    try:
-        train_avae_MNIST(batch_size = 64,
-                        z_dim = 16,
-                        primal_lr = 1e-3,
-                        adv_lr = 1e-4,
-                        epochs = 30,
-                        image_size = 16,
-                        input_channels = 1,
-                        results_folder = "results/avae_binary_mnist_1",
-                        binary = True,
-                        decoder_features = 16,
-                        encoder_features = 16)
-    except:
-        print('Error in AVAE Binary MNIST Setup 1')
-
-    print('Setup 2')
-    try:
-        train_avae_MNIST(batch_size = 64,
-                        z_dim = 32,
-                        primal_lr = 1e-3,
-                        adv_lr = 1e-4,
-                        epochs = 30,
-                        image_size = 32,
-                        input_channels = 1,
-                        results_folder = "results/avae_binary_mnist_2",
-                        binary = True,
-                        decoder_features = 32,
-                        encoder_features = 32)
-    except:
-        print('Error in AVAE Binary MNIST Setup 2')
-
-    print('Setup 3')
-    try:
-        train_avae_MNIST(batch_size = 64,
-                        z_dim = 32,
-                        primal_lr = 1e-4,
-                        adv_lr = 5e-4,
-                        epochs = 40,
-                        image_size = 32,
-                        input_channels = 1,
-                        results_folder = "results/avae_binary_mnist_3",
-                        binary = True,
-                        decoder_features = 32,
-                        encoder_features = 32)
-    except:
-        print('Error in AVAE Binary MNIST Setup 3')
-
-    print('Setup 4')
-    try:
-        train_avae_MNIST(batch_size = 64,
-                        z_dim = 64,
-                        primal_lr = 1e-3,
-                        adv_lr = 1e-4,
-                        epochs = 40,
-                        image_size = 32,
-                        input_channels = 1,
-                        results_folder = "results/avae_binary_mnist_4",
-                        binary = True,
-                        decoder_features = 64,
-                        encoder_features = 64)
-    except:
-        print('Error in AVAE Binary MNIST Setup 4')
+        except Exception as e:
+            print(f'Error in Setup {i + 1}: {e}')
