c5ffb5c079a4938cbcc1505d75c19d5939a0e4d1
Vincent Longpre <vincent.longpre1729@gmail.com>
2023-12-17 13:04:45 -0500

    Fixed bugs

--- make_visualizations.py
+++ make_visualizations.py
@@ -12,9 +12,11 @@ from pathlib import Path
 device = "cuda" if torch.cuda.is_available() else "cpu"
 data_root = "./data"
 result_root = "./VariNet/plots"
-model_root = "./VariNet/trained_models"
+model_root = "./VariNet/trained_models/"
 
-params = pd.read_json(model_root + 'avae_best/params.json')
+with open(model_root + 'avae_best/params.json', 'r') as json_file:
+    params = json.load(json_file)
+
 locals().update(params)
 download = True
 
@@ -22,6 +24,7 @@ download = True
 if __name__ == "__main__":
     train_dataloader, test_dataloader = binary_mnist_dataloaders(data_root, batch_size=batch_size, image_size=image_size, download=download)
 
+    # Load models
     avae = MnistAVAE(in_channels=input_channels,
                     input_size=image_size,
                     z_dim=z_dim,
@@ -37,8 +40,10 @@ if __name__ == "__main__":
             decoder_features=decoder_features,
             encoder_features=encoder_features,
             device=device)
+    avae.load_state_dict(torch.load(model_root + 'vae_comparison/model.pth'))
     vae.to(device)
 
-    visualize_latent_space(vae, test_dataloader, device, result_root)
-    visualize_latent_space(avae, test_dataloader, device, result_root)
+    # Visualize latent space
+    visualize_latent_space(vae, test_dataloader, device, result_root+"vae_latent.png")
+    visualize_latent_space(avae, test_dataloader, device, result_root+"avae_latent.png")
 
